6f33fdc82aaab285bb3eaf497d43a707adedf042
CodeHz <CodeHz@Outlook.com>
2017-02-14 14:28:04 +0800

    Feature: Open System Settings Application Detail Page For Container

--- app/src/main/java/one/codehz/container/ShortcutActivity.kt
+++ app/src/main/java/one/codehz/container/ShortcutActivity.kt
@@ -1,6 +1,8 @@
 package one.codehz.container
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import one.codehz.container.ext.virtualCore
@@ -8,6 +10,7 @@ import one.codehz.container.ext.virtualCore
 class ShortcutActivity : Activity() {
     companion object {
         val CLEAR_ALL = "one.codehz.container.CLEAR_ALL"
+        val SELF_DETAIL = "one.codehz.container.SELF_DETAIL"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,11 @@ class ShortcutActivity : Activity() {
 
         when (intent.action) {
             CLEAR_ALL -> virtualCore.killAllApps()
+            SELF_DETAIL -> {
+                startActivity(Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                    data = Uri.parse("package:$packageName")
+                })
+            }
         }
 
         finish()
