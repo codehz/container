eb6e5a5f356ba5b0e2b8ac61c30e225f8d99970b
CodeHz <CodeHz@Outlook.com>
2017-02-17 21:57:14 +0800

    Feature: Fake Device Id and Bluetooth Address

--- app/src/main/java/one/codehz/container/delegate/MyPhoneInfoDelegate.kt
+++ app/src/main/java/one/codehz/container/delegate/MyPhoneInfoDelegate.kt
@@ -2,12 +2,23 @@ package one.codehz.container.delegate
 
 import android.util.Log
 import com.lody.virtual.client.hook.delegate.PhoneInfoDelegate
+import one.codehz.container.ext.sharedPreferences
 
 class MyPhoneInfoDelegate : PhoneInfoDelegate {
     override fun getDeviceId(oldDeviceId: String): String {
         Log.d("MyPhoneInfoDelegate", "WANT GET DEVICE ID $oldDeviceId")
+        sharedPreferences.getString("privacy_phone_device_id", "").apply {
+            if (isNotEmpty())
+                return this
+        }
         return (oldDeviceId.toLong() + 1).toString()
     }
 
-    override fun getBluetoothAddress(oldAddress: String) = oldAddress
+    override fun getBluetoothAddress(oldAddress: String): String {
+        sharedPreferences.getString("privacy_phone_bluetooth_address", "").apply {
+            if (isNotEmpty())
+                return this
+        }
+        return oldAddress
+    }
 }
